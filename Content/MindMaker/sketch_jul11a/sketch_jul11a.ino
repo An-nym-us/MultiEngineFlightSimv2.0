d3b57f0057f10b334cd2a4865edc8074d99e1b4f
Jonathan <jlgunderson2@gmail.com>
2022-07-14 11:28:59 -0600

    777

--- Content/MindMaker/sketch_jul11a/sketch_jul11a.ino
+++ Content/MindMaker/sketch_jul11a/sketch_jul11a.ino
@@ -11,8 +11,12 @@
   #include <String.h>
   
   
-  Servo myservo;  // create servo object to control a servo
-  // twelve servo objects can be created on most boards
+  Servo servoFront;
+  Servo servoLeft;
+  Servo servoRight;
+  Servo servoRear;// create servo object to control a servo
+  
+
   int sensorValue = 0;        // value read from the pot
   int outputValue = 0;        // value output to the PWM (analog out)
   int UserInputPin = 5;       // This variable turns on an LED to the pin specified by the user
@@ -22,93 +26,72 @@
   int pos = 0;    // variable to store the servo position
   
   void setup() {
-    myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+    servoFront.attach(9);  // attaches the servo on pin 9 to the servo object
+    servoLeft.attach(10);  // attaches the servo on pin 9 to the servo object
+    servoRight.attach(11);  // attaches the servo on pin 9 to the servo object
+    servoRear.attach(6);  // attaches the servo on pin 9 to the servo object
+    
     Serial.begin(9600);
     while (! Serial); // Wait untilSerial is ready - Leonardo
-    Serial.println("Enter LED Number 0 to 7 or 'x' to clear");
     // initialize serial communications at 9600 bps:
-    Serial.begin(9600);
+      Serial.begin(9600);
   }
   
-  void loop() {
+  void loop() 
+  {
     updateServos();
-
   }
+
+
   
   void updateServos()
   {
-     Serial.print("sensor = ");
-    Serial.print(sensorValue);
+    String output;
+    char pass [] =  "777,777,777,777";
     
-  
-  String output;
-  char pass [] =  "777,777,777,777";
     
-  
-     if (Serial.available())
-      {
-          output = Serial.readStringUntil('\n');
-       // Serial.println("\t output = ");
-         // Serial.println(output);
-  
-          
-      //char buf[sizeof(sz)];
-     // serialResponse.toCharArray(buf, sizeof(buf));
-     // char *p = buf;
-      //char *str;
+    if (Serial.available())
+    {
+      output = Serial.readStringUntil('\n');
       output.toCharArray(pass, 16);
       String Out1;
       String Out2;
-   for (int i = 0; i <= 2; i++)
-   {
-      String temp = String(pass[i]);
-      Out1.concat(temp); 
-   }
-    for (int i = 4; i <= 6; i++)
-   {
-      String temp = String(pass[i]);
-      Out2.concat(temp); 
-  }
-        
-    float Fnum = Out1.toFloat();
-    float Bnum = Out2.toFloat();
-    
-    myservo.write(Fnum);
-    /*
-        for (pos = 0; pos <= 180; pos += Fnum) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-          myservo.write(pos);              // tell servo to go to position in variable 'pos'
-          delay(15);                       // waits 15ms for the servo to reach the position
-        }
-  
-        
-        for (pos = 180; pos >= 0; pos -= Bnum) { // goes from 180 degrees to 0 degrees
-          myservo.write(pos);              // tell servo to go to position in variable 'pos'
-          delay(15);                       // waits 15ms for the servo to reach the position
-        }
-  */
-        
-        /*
-        char ch = Serial.read();
-        if (ch >= '0' && ch <= '7')
+      String Out3;
+      String Out4;
+      
+      for (int i = 0; i <= 2; i++)
         {
-          
-          int led = ch - '0';
-          UserInputPin=led;
-          bitSet(leds, led);
-          //updateLEDsON();
-          Serial.print("Turned on LED ");
-          Serial.println(led);
+          String temp = String(pass[i]);
+          Out1.concat(temp); 
         }
-        if (ch == 'x')
+      for (int i = 4; i <= 6; i++)
         {
-          leds = 0;
-          //updateLEDsOFF();
-          Serial.println("Cleared");
+          String temp = String(pass[i]);
+          Out2.concat(temp); 
         }
-        */
+      for (int i = 8; i <= 10; i++)
+        {
+          String temp = String(pass[i]);
+          Out3.concat(temp); 
+        }
+      for (int i = 12; i <= 14; i++)
+        {
+          String temp = String(pass[i]);
+          Out4.concat(temp); 
+        }
+            
+        float Frontnum = Out1.toFloat();
+        float Rightnum = Out2.toFloat();
+        float Lightnum = Out3.toFloat();
+        float Rearnum = Out4.toFloat();
+        
+      servoFront.write(Frontnum);
+      servoLeft.write(Rightnum);
+      servoRight.write(Lightnum);
+      servoRear.write(Rearnum);
+   
       }
-    //delay(4);
+
   
   }
  
---
+++ Source/MultiEngineFlightSim/Public/AddLeadingZeros.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AddLeadingZeros.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIENGINEFLIGHTSIM_API UAddLeadingZeros : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+   UFUNCTION(BlueprintCallable)
+      static FString AddLeadingZeros(const int32& UglyNumber, const int Length);
+
+	
+};
